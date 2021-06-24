fa75abfcdfa3a66eb59fb9df05a1886bb4c8fd40
AbeTeshe <abebeteshome37@gmail.com>
2021-06-24 17:14:00 +0300

    Dash board admin

--- src/App.js
+++ src/App.js
@@ -3,16 +3,44 @@ import Topbar from "./components/topbar/Topbar";
 import Sidebar from "./components/sidebar/Sidebar";
 import "./app.css";
 import Home from "./pages/home/Home";
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom"; 
+import UserList from './pages/userList/UserList';
+import User from './pages/user/User';
+import NewUser from './pages/newUser/NewUser';
+import ProductList from './pages/productList/ProductList';
+import Product from './pages/product/Product';
+
 function App() {
   return (
-    <div className="App">
-      <Topbar />
+    <Router>
+      <Topbar /> 
       <div className ="container">
          <Sidebar />
-         <Home />
-
+         <Switch>
+           <Route exact path="/">
+              <Home />
+           </Route>
+           <Route path="/users">
+              <UserList />
+           </Route>
+           <Route path="/user/:userId">
+              <User/>
+           </Route>
+           <Route path="/newUser">
+              <NewUser/>
+           </Route>
+           <Route path="/products">
+              <ProductList />
+           </Route>
+           <Route path="/product/:prodcutId">
+              <Product/>
+           </Route>
+           <Route path="/newProduct">
+              <NewUser/>
+           </Route>
+         </Switch>
       </div>
-    </div>
+    </Router>
   );
 }
 
--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import {LineStyle, Timeline, TrendingUp,PermIdentity,
     Storefront,
     AttachMoney,
@@ -34,14 +35,19 @@ export default function Sidebar() {
                 <div className="sidebarMenu">
                     <h3 className="sidebarTitle">Quick Menu</h3>
                     <ul className="sidebarList">
-                        <li className="sidebarListItem active">
-                            <PermIdentity className="sidebarIcon"/>
-                            Users
-                        </li>
-                        <li className="sidebarListItem">
-                            <Storefront className="sidebarIcon"/>
-                            Products
-                        </li>
+                        <Link to="/users" className="link">
+                            <li className="sidebarListItem active">
+                                <PermIdentity className="sidebarIcon"/>
+                                Users
+                            </li>
+                        </Link>
+                        <Link to="/products" className="link">
+                            <li className="sidebarListItem">
+                                <Storefront className="sidebarIcon"/>
+                                Products
+                            </li>
+                        </Link>
+
                         <li className="sidebarListItem">
                             <AttachMoney className="sidebarIcon"/>
                             Transactions
